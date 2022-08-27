e65cee36490f2e2bfa19aa1fb07b1c670c62f765
hozsocs <hozsocs@gmail.com>
2022-08-27 17:08:40 +0200

    tesztelés előtt

--- main.js
+++ main.js
@@ -23,22 +23,106 @@ let calculator="0";
 let actualpressedbutton="";
 let lastbuttonisoperator= false;
 let pressedbuttonisoperator = false;
+let egualsignpressed =false;
 
 // Függvények definíciója
 // Egyenlőségjel megnyomásakor kiszámolja az eredményt!
-const egualfunction=() => {
-  let actualoperator="";
-  let actualnumber="";
-  let actualnumber1="";
+const firstoperatorposition=() =>{
+goodindex=-1;
   for (let index = 0; index < calculator.length-1; index++) {
-    ((pressedbuttonisnumber(calculator.charAt(index))) || 
-      (calculator.charAt(index))===".") ? {
-        actualnumber1 = actualnumber;
-        actualnumber = actualnumber + calculator.charAt(index)} :
-        actualoperator=(calculator.slice(index, index+1))
+    if ((calculator.charAt(index))==="+") {goodindex=index; break};
+    if ((calculator.charAt(index))==="-") {goodindex=index; break};
+    if ((calculator.charAt(index))==="*") {goodindex=index; break};
+    if ((calculator.charAt(index))==="/") {goodindex=index; break};
+     
+}
+return goodindex;
+} 
+
+const egualfunction=() => {
+ 
+  let actualoperator="";
+  let lastoperator="";
+  let actualnumber="";
+  let actualnumbertonumber=0;
+  let lastnumber=0;
+  let sum=0;
+  let firstoperator=0
+  //Az első szám és operátor!!
+  firstoperator=firstoperatorposition();
+  (firstoperator===-1) ? sum=parseFloat(calculator): 
+    sum=parseFloat(calculator.slice(0,firstoperator));
+    lastoperator=calculator.slice(firstoperator,firstoperator+1);
+  
+
+  //többi szám, műveleti jelek, összesítés
+  for (let index = firstoperator+1; index < calculator.length; index++) {
+   
+    if ((pressedbuttonisnumber(calculator.charAt(index))) || 
+      (calculator.charAt(index))===".") {
+
+       actualnumber = actualnumber + calculator.charAt(index)
+       
+      } 
+    else {
+      
+      actualoperator=(calculator.charAt(index));
+      actualnumbertonumber=parseFloat(actualnumber);
+     
+        switch (lastoperator) {
+          case "+": sum=sum+actualnumbertonumber;
+          break;
+
+          case "-": sum=sum-actualnumbertonumber;
+          break;
+          
+          case "*": sum=sum*actualnumbertonumber;
+          break;
+
+          case "/": sum=sum/actualnumbertonumber;
+          break;            
+
+          default: "";
+            break;
+        } 
+         
+       if ((actualoperator==="")) {} else {
+       
+        lastoperator=actualoperator;
+        lastnumber=parseFloat(actualnumber);
+        actualnumber="";
+        actualoperator="";
+        } 
+        console.log(sum); 
+    }
     
-  }
-};
+  }; /*a for ciklus vége, utolsó művelet elvégzése*/
+
+   switch (lastoperator) {
+        case "+": sum=sum+actualnumber;
+        break;
+
+        case "-": sum=sum-actualnumber;
+        break;
+        
+        case "*": sum=sum*actualnumber;
+        break;
+
+        case "/": sum=sum/actualnumber;
+        break;            
+
+        default: "";
+          break;
+      }      
+   // Eredmény kiíratása, számolás folytatásának biztosítása
+   calculator=sum.toPrecision(10).toString(10);
+   displayfunction();
+  
+  egualsignpressed=true;
+  
+    }  
+  
+
 
 /* annak vizsgálata, hogy egy számon belül hányszor 
 nyomták meg a tizedes vesszőt */
@@ -68,11 +152,12 @@ if (calculator.length>12)  {
 // A lenyomott gomb szám-e?
 const pressedbuttonisnumber = (sign) => {
 let signboolean=false;
-   signboolean=((sign==="2") || (sign==="2") || (sign==="3") 
+   signboolean=((sign==="1") || (sign==="2") || (sign==="3") 
   || (sign==="4") || (sign==="5") || (sign==="6") || (sign==="7") 
   || (sign==="8") || (sign==="9") || (sign==="0")); 
 return signboolean;
 };
+
 
  
 // Az előző lenyomott billentyű vizsgálata
@@ -119,18 +204,22 @@ var pressedbutton = (sign) => {
   
     case "+":
       pressedbuttonisoperator=true;
+      egualsignpressed=false;
       break;
     
       case "-":
       pressedbuttonisoperator=true;
+      egualsignpressed=false;
       break;
   
       case "*":
       pressedbuttonisoperator=true;
+      egualsignpressed=false;
       break;
   
       case "/":
       pressedbuttonisoperator=true;
+      egualsignpressed=false;
       break;
   
     default: 
@@ -146,6 +235,7 @@ lastbuttonisoperator=false;
 lastpressedbutton(calculator.slice(-1));  
 sign = pressedbutton(sign);
 
+
   if (lastbuttonisoperator && pressedbuttonisoperator) {sign=""} 
   else if ((calculator==="0") && (sign==="0")) {
     calculator="0";
@@ -157,6 +247,12 @@ sign = pressedbutton(sign);
     (pressedbuttonisnumber(calculator.slice(-1))===false))
     {sign=""; 
     }
+  else if (((pressedbuttonisnumber(sign)===true) || (sign===".")) && 
+    (egualsignpressed===true))
+    {
+      sign=""; 
+    }
+  
   else if ((sign===".") && calculator.includes(".")) 
   { 
     sign=pointinvestigation(calculator);
@@ -167,6 +263,7 @@ sign = pressedbutton(sign);
   calculator=calculator+sign;
   }
     displayfunction();
+    
 }
 
 // kattintások figyelése
